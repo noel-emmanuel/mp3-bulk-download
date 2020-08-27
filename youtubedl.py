6e04e74e21126a24ca9a8c13a58e2b69f48eb3f3
noel-emmanuel <helloitsnoel@gmail.com>
2020-08-27 10:20:12 +1000

    mvp done

--- youtubedl.py
+++ youtubedl.py
@@ -10,7 +10,6 @@ from moviepy.editor import *
 # print("Downloading... " + yt.title)
 # print("Downloaded! to " + path)
 
-# changes ----------------------------------------------------
 
 print("Reading link in links.txt...")
 with open('links.txt') as f:
@@ -23,23 +22,23 @@ with open('links.txt') as f:
         # - convert video to mp3
         # - delete video
 
-        yt_link = line
-        yt = YouTube(yt_link)
+        # yt_link = line
+        yt = YouTube(line)
         song_name = yt.title
         # terminal output to confirm download has started
-        path1 = '/mnt/e/'
+        path1 = '/mnt/e/python_mp3'
         print("Starting download for: " + song_name + " and saving to: " + path1)
 
         # downloading audio only *new change*
         audio_streams = yt.streams.all()
-        print(audio_streams[0])
-        audio_mp4 = audio_streams[0]
+        audio_mp4 = audio_streams[1]
+        print(audio_mp4)
         # downloading mp4 audio only file to a path that will be deleted later
         temp_path = audio_mp4.download(path1)   
         mp4_file = temp_path
         print("Converting audio-only video to an mp3 format...")
         # creating a path for audio file to be saved at 
-        song_path = path1 + "/" +  song_name + ".mp3"
+        song_path = path1 + "/" + song_name + ".mp3"
         mp3_file = song_path
         # conversion from mp4 to mp3
         videoclip = VideoFileClip(mp4_file) # create a videoclip object
@@ -49,4 +48,4 @@ with open('links.txt') as f:
         videoclip.close()
 
         os.remove(temp_path)
-        print("Finished! Downloaded to " + path1)     
+        print("Finished! Downloaded to " + path1 + "---------------------------")     
